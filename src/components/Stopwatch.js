1d14fb1c17521cf46c7740e1ea0b9bdc898a85b2
Nick-Hericks <nickhericks@gmail.com>
2019-04-18 13:02:11 +0200

    Update seconds

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -4,24 +4,48 @@ class Stopwatch extends Component {
 
 	state = {
 		isRunning: false,
-
+		elapsedTime: 0,
+		previousTime: 0
 	};
 
+	componentDidMount() {
+		console.log('stopwatch mounted')
+		this.intervalID = setInterval(() => this.tick(), 100)
+	}
+
 	tick = () => {
-		
+		// console.log('ticking...')
+		if (this.state.isRunning) {
+			const now = Date.now();
+			this.setState( prevState => ({
+				previousTime: now,
+				elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+			}));
+		}
 	}
 
 	handleStopwatch = () => {
-		this.setState({
-			isRunning: !this.state.isRunning
-		});
+		this.setState( prevState => ({
+			isRunning: !prevState.isRunning
+		}));
+		if (!this.state.isRunning) {
+			console.log('starting');
+			this.setState({
+				previousTime: Date.now()
+			});
+		}
 	}
 
 	render() {
+
+		const seconds = Math.floor(this.state.elapsedTime / 1000);
+
 		return(
 			<div className="stopwatch">
 				<h2>Stopwatch</h2>
-				<span className="stopwatch-time">0</span>
+				<span className="stopwatch-time">
+					{seconds}
+				</span>
 
 				<button onClick={this.handleStopwatch}>
 					{ this.state.isRunning ? 'Stop' : 'Start' }
