a299e8ca91cbaf35417aba7235d847c09345fbc1
Nick-Hericks <nickhericks@gmail.com>
2019-04-18 12:45:28 +0200

    Add handleStopwatch function

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+
+	state = {
+		isRunning: false,
+
+	};
+
+	tick = () => {
+		
+	}
+
+	handleStopwatch = () => {
+		this.setState({
+			isRunning: !this.state.isRunning
+		});
+	}
+
 	render() {
 		return(
 			<div className="stopwatch">
 				<h2>Stopwatch</h2>
 				<span className="stopwatch-time">0</span>
 
-				<button>Start</button>
+				<button onClick={this.handleStopwatch}>
+					{ this.state.isRunning ? 'Stop' : 'Start' }
+				</button>
 
 
 				<button>Reset</button>
